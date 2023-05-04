e70996ee1a60626876efaa8c602d527e33cbb905
Mikhail Novik <novik.misha.1@gmail.com>
2023-05-04 17:50:46 +0300

    fixing wotlk

--- Glass/Components/GradientBackground.lua
+++ Glass/Components/GradientBackground.lua
@@ -7,33 +7,33 @@ end
 
 function GradientBackgroundMixin:SetGradientBackground(leftWidth, rightWidth, color, opacity)
   if self.leftBg == nil then
-    self.leftBg = self:CreateTexture(nil, "BACKGROUND")
+    self.leftBg = self:CreateTexture('', "BACKGROUND")
     self.leftBg:SetPoint("TOPLEFT")
     self.leftBg:SetPoint("BOTTOMLEFT")
     self.leftBg:SetColorTexture(1, 1, 1, 1)
   end
   self.leftBg:SetWidth(leftWidth)
-  self.leftBg:SetGradientAlpha(
+  self.leftBg:SetGradient(
     "HORIZONTAL",
-    color.r, color.g, color.b, 0,
-    color.r, color.g, color.b, opacity
+    CreateColor(color.r, color.g, color.b, 0),
+    CreateColor(color.r, color.g, color.b, opacity)
   )
 
   if self.rightBg == nil then
-    self.rightBg = self:CreateTexture(nil, "BACKGROUND")
+    self.rightBg = self:CreateTexture('', "BACKGROUND")
     self.rightBg:SetPoint("TOPRIGHT")
     self.rightBg:SetPoint("BOTTOMRIGHT")
     self.rightBg:SetColorTexture(1, 1, 1, 1)
   end
   self.rightBg:SetWidth(rightWidth)
-  self.rightBg:SetGradientAlpha(
+  self.rightBg:SetGradient(
     "HORIZONTAL",
-    color.r, color.g, color.b, opacity,
-    color.r, color.g, color.b, 0
+    CreateColor(color.r, color.g, color.b, opacity),
+    CreateColor(color.r, color.g, color.b, 0)
   )
 
   if self.centerBg == nil then
-    self.centerBg = self:CreateTexture(nil, "BACKGROUND")
+    self.centerBg = self:CreateTexture('', "BACKGROUND")
     self.centerBg:SetPoint("TOPLEFT", self.leftBg, "TOPRIGHT")
     self.centerBg:SetPoint("BOTTOMRIGHT", self.rightBg, "BOTTOMLEFT")
   end
